5fdc823acac688474af5b9b863d4da5ce9ac2d73
JavidPack <javidpack@gmail.com>
2018-01-06 08:15:42 -0700

    Pr/1 (#2)
    * Initial refactor of the main mod class code.
    - Break out major portions of logic into helper functions
    - Pre-configure data in rough db-like way to reduce code duplication
    
    * More general refactoring to clean things up and start modularizing the code
    
    * remove files

---
+++ Models/MinionModel.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+using Terraria.ID;
+
+namespace SummonersAssociation.Models
+{
+    public class MinionModel 
+    {
+        public int ItemID { get; set; }
+        public int BuffID { get; set; }
+        public List<int> ProjectileIDs { get; set; }
+
+        public MinionModel(int itemID, int buffID, List<int> projectileIDs) 
+        {
+            this.ItemID = itemID;
+            this.BuffID = buffID;
+            this.ProjectileIDs = projectileIDs;
+        }
+    }
+}
