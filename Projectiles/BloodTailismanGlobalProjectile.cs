6e39940ddaeebe178eb87e9594f48689f063ebeb
direwolf420 <amakejk1@web.de>
2020-06-13 19:05:27 +0200

    BloodTalismanGlobalProjectile cleanup

--- Projectiles/BloodTailismanGlobalProjectile.cs
+++ Projectiles/BloodTailismanGlobalProjectile.cs
@@ -7,43 +7,11 @@ namespace SummonersAssociation.Projectiles
 {
 	public class BloodTalismanGlobalProjectile : GlobalProjectile
 	{
-		//public override void SetDefaults(Projectile projectile)
-		//{
-		//	// hornet						imp						optic					pygmy						ufo						sharkba					minicell	
-		//	if (projectile.type == ProjectileID.HornetStinger|| projectile.type == ProjectileID.MiniRetinaLaser|| projectile.type == ProjectileID.ImpFireball || projectile.type == ProjectileID.PygmySpear || projectile.type == ProjectileID.UFOLaser || projectile.type == ProjectileID.MiniSharkron || projectile.type == ProjectileID.StardustCellMinionShot)
-		//	{
-		//		Projectile p = null;
-		//		//if pro owner is channel and 
-		//		for (int j = 0; j < Main.maxProjectiles; j++)
-		//		{
-		//			if (Main.projectile[j].active && Main.projectile[j].owner == projectile.owner)
-		//			{
-		//				if (Main.projectile[j].type == mod.ProjectileType("BloodTalismanTargetProjectile"))
-		//				{
-		//					p = Main.projectile[j];
-		//					break;
-		//				}
-		//			}
-		//		}
-		//		if (p != null)
-		//		{
-		//			if (Vector2.Distance(p.position, projectile.position) < 100)
-		//			{
-		//				projectile.damage *= 100;
-		//				projectile.knockBack *= 100;
-		//			}
-		//		}
-		//	}
-		//}
-
 		public override void ModifyHitNPC(Projectile projectile, NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection) {
-			// if intersecting with bloodtalismantarget
-			//projectile.type == ProjectileID.HornetStinger || projectile.type == ProjectileID.MiniRetinaLaser || projectile.type == ProjectileID.ImpFireball || projectile.type == ProjectileID.PygmySpear || projectile.type == ProjectileID.UFOLaser || projectile.type == ProjectileID.MiniSharkron || projectile.type == ProjectileID.StardustCellMinionShot
+			//If intersecting with BloodTalismanTargetProjectile, increase damage and knockback
 			if (projectile.minion || ProjectileID.Sets.MinionShot[projectile.type]) {
 				Projectile talismanProj = null;
-				BloodTalismanTargetProjectile talisman = null;
 				int talismanType = ProjectileType<BloodTalismanTargetProjectile>();
-				//if pro owner is channel and 
 				for (int j = 0; j < Main.maxProjectiles; j++) {
 					Projectile p = Main.projectile[j];
 					if (p.active && p.owner == projectile.owner && p.type == talismanType) {
@@ -51,29 +19,30 @@ namespace SummonersAssociation.Projectiles
 						break;
 					}
 				}
+
 				if (talismanProj != null) {
-					talisman = talismanProj.modProjectile as BloodTalismanTargetProjectile;
+					var talisman = talismanProj.modProjectile as BloodTalismanTargetProjectile;
 					int alpha = talisman.alphaTimer;
 					if (talismanProj.DistanceSQ(target.Center) < 50 * 50) {
 						// 155 max
 						// 10x damage
-						if (alpha == 155) {
+						if (alpha == BloodTalismanTargetProjectile.alphaFinal) {
 							damage *= 10;
 							knockback *= 2.5f; // 1.5
 						}
 						// 5 to almost 10x
 						else if (alpha < 170) {
 							damage = (int)((5 + ((170 - alpha) / 5f)) * damage); // 5 to 10
-							knockback *= (1.75f + .75f * ((195 - alpha) / 25f)); // .75 to 1.5
+							knockback *= 1.75f + .75f * ((195 - alpha) / 25f); // .75 to 1.5
 						}
 						//
 						else if (alpha < 195) {
 							damage = (int)((2.5 + ((195 - alpha) / 10f)) * damage);
-							knockback *= ((1.3f + .3f * ((195 - alpha) / 25f)));
+							knockback *= 1.3f + .3f * ((195 - alpha) / 25f);
 						}
 						else if (alpha < 220) {
 							damage = (int)((1.25 + ((220 - alpha) / 20f)) * damage);
-							knockback *= ((1.15f + .15f * ((195 - alpha) / 25f)));
+							knockback *= 1.15f + .15f * ((195 - alpha) / 25f);
 						}
 						else /*if (alpha < 255)*/
 						{
